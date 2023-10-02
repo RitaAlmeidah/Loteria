755333ace4671133edb2e4cd70b96b8d49fa7b7e
rita_@Almeida <rita_aja92@hotmail.com>
2023-10-02 11:37:41 -0300

    Corrigido anotacao do teste e adicionado novo job coverage

--- src/test/java/com/mega/lottery/util/UtilTest.java
+++ src/test/java/com/mega/lottery/util/UtilTest.java
@@ -2,7 +2,10 @@ package com.mega.lottery.util;
 
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class UtilTest {
 
     @Test
